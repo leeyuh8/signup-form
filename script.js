ac25aadb28998999423bdccedc9c11a62c771e85
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2023-12-20 13:10:22 -0800

    Add email placeholder and validation function

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const registerBtn = document.querySelector("button");
-registerBtn.addEventListener('click', stylePassInputs);
+registerBtn.addEventListener('click', validateInputs);
 
 const passOriginalInput = document.querySelector("#password");
 const passConfirmInput = document.querySelector("#password-confirm");
 const passInputs = [passOriginalInput, passConfirmInput];
+const emailInput = document.querySelector("#email");
 const para = document.querySelector(".pass-warning");
+const form = document.querySelector("form");
 
-function stylePassInputs() {
-  let validationResult = validatePasswords();
+function validateInputs() {
+  let checkEmailResult = checkEmail();
+  let validationResult = checkPasswords();
   toggleStyleClass(validationResult);
+  if (validationResult === true) {
+    changeEventListener();
+  }
 }
 
-function validatePasswords() {
+function checkEmail() {
+  const emailValue = emailInput.value;
+  const emailRegex = /^.+@.+\..+$/;
+
+  return emailRegex.test(emailValue);
+}
+
+function checkPasswords() {
   const passOriginalValue = passOriginalInput.value;
   const passConfirmValue = passConfirmInput.value;
 
@@ -23,13 +36,24 @@ function toggleStyleClass(validationResult) {
     input.classList.remove('invalid-input');
   });
   para.textContent  = "";
-  passConfirmInput.removeEventListener('input', stylePassInputs);
+  passConfirmInput.removeEventListener('input', validateInputs);
 
   if (validationResult === false) {
     passInputs.forEach((input) => {
       input.classList.add('invalid-input');
     });
     para.textContent = "* Passwords do not match";
-    passConfirmInput.addEventListener('input', stylePassInputs);
+    passConfirmInput.addEventListener('input', validateInputs);
   };
+}
+
+function changeEventListener() {
+  registerBtn.removeEventListener('click', validateInputs);
+  registerBtn.addEventListener('click', () => {
+    if (form.checkValidity()) {
+      form.submit()
+    } else {
+      window.alert("You must fill all required fields correctly.");
+    }
+});
 }
