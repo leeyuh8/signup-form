a43e4bd803bb41e852c7a072155869fafe3fd679
Strallia Chao <134454347+leeyuh8@users.noreply.github.com>
2023-12-20 13:52:51 -0800

    Manage display and removal of invalid styles and reorganize script

--- script.js
+++ script.js
@@ -1,29 +1,49 @@
-const registerBtn = document.querySelector("button");
-registerBtn.addEventListener('click', validateInputs);
+// SET DEFAULT ACTIONS
+const emailInput = document.querySelector("#email");
+const emailWarningPara = document.querySelector(".email-warning");
+emailInput.addEventListener('input', () => {
+  let result = checkEmail();
+  if (result === false) {
+    displayInvalidEmailStyles();
+  } else if (result === true) {
+    removeInvalidEmailStyles();
+  }
+});
 
 const passOriginalInput = document.querySelector("#password");
 const passConfirmInput = document.querySelector("#password-confirm");
 const passInputs = [passOriginalInput, passConfirmInput];
-const emailInput = document.querySelector("#email");
-const para = document.querySelector(".pass-warning");
-const form = document.querySelector("form");
+const passWarningPara = document.querySelector(".pass-warning");
+passConfirmInput.addEventListener('input', () => {
+  let result = checkPasswords();
+  if (result === false) {
+    displayInvalidPassStyles();
+  } else if (result === true) {
+    removeInvalidPassStyles();
+  }
+});
 
+const form = document.querySelector("form");
+const registerBtn = document.querySelector("button");
+registerBtn.addEventListener('click', validateInputs);
 function validateInputs() {
   let checkEmailResult = checkEmail();
-  let validationResult = checkPasswords();
-  toggleStyleClass(validationResult);
-  if (validationResult === true) {
-    changeEventListener();
-  }
+  let checkPassResult = checkPasswords();
+  if (checkEmailResult && checkPassResult) {
+    form.submit();
+  } else if (checkEmailResult === false || checkPassResult === false) {
+    window.alert("All required fields must be filled in correctly.");
+  };
 }
 
+
+// REFERENCE FUNCTIONS
 function checkEmail() {
   const emailValue = emailInput.value;
   const emailRegex = /^.+@.+\..+$/;
 
   return emailRegex.test(emailValue);
 }
-
 function checkPasswords() {
   const passOriginalValue = passOriginalInput.value;
   const passConfirmValue = passConfirmInput.value;
@@ -31,29 +51,24 @@ function checkPasswords() {
   return passOriginalValue === passConfirmValue ? true : false;
 }
 
-function toggleStyleClass(validationResult) {
+function displayInvalidEmailStyles() {
+  emailInput.classList.add('invalid-input');
+  emailWarningPara.textContent = "* Invalid email. Use the format email@address.com";
+}
+function displayInvalidPassStyles() {
+  passInputs.forEach((input) => {
+    input.classList.add('invalid-input');
+  });
+  passWarningPara.textContent = "* Passwords must match";
+}
+
+function removeInvalidEmailStyles() {
+  emailInput.classList.remove('invalid-input');
+  emailWarningPara.textContent = "";
+}
+function removeInvalidPassStyles() {
   passInputs.forEach((input) => {
     input.classList.remove('invalid-input');
   });
-  para.textContent  = "";
-  passConfirmInput.removeEventListener('input', validateInputs);
-
-  if (validationResult === false) {
-    passInputs.forEach((input) => {
-      input.classList.add('invalid-input');
-    });
-    para.textContent = "* Passwords do not match";
-    passConfirmInput.addEventListener('input', validateInputs);
-  };
-}
-
-function changeEventListener() {
-  registerBtn.removeEventListener('click', validateInputs);
-  registerBtn.addEventListener('click', () => {
-    if (form.checkValidity()) {
-      form.submit()
-    } else {
-      window.alert("You must fill all required fields correctly.");
-    }
-});
+  passWarningPara.textContent = "";
 }
